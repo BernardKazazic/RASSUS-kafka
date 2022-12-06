eb7d419c77dcc9ac7c62ef918e010fc64862cd5f
Bernard Kazazić <bernard.kazazic@commondo.eu>
2022-12-06 16:49:06 +0100

    CoordinatorNode added descriptive comments

--- CoordinatorNode/src/main/java/rassus/lab2/CoordinatorNode.java
+++ CoordinatorNode/src/main/java/rassus/lab2/CoordinatorNode.java
@@ -16,6 +16,7 @@ public class CoordinatorNode {
     private static String TOPIC = "Command";
 
     public static void main(String[] args) {
+        // create producer
         Properties producerProperties = new Properties();
         producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -23,26 +24,31 @@ public class CoordinatorNode {
 
         Producer<String, JSONObject> producer = new org.apache.kafka.clients.producer.KafkaProducer<>(producerProperties);
 
+        // command input
         Scanner sc = new Scanner(System.in);
-
         while (true) {
             System.out.print("Send a command: ");
             String command = sc.nextLine().toUpperCase();
 
+            // check if command is valid
             if(!command.equals("START") && !command.equals("STOP")) {
                 System.out.println("Valid commands are:\nSTART - starts a group of nodes\nSTOP - stops and shuts down all nodes\n\n");
                 continue;
             }
             System.out.println("Sending " + command + " command.");
 
+            // create json object for command
             JSONObject jsonCommand = new JSONObject();
             jsonCommand.put("command", command);
 
+            // create producer record for command topic
             ProducerRecord<String, JSONObject> record = new ProducerRecord<>(TOPIC, null, jsonCommand);
 
+            // send producer record to kafka server
             producer.send(record);
             producer.flush();
 
+            // stop coordinator node after it sent stop command
             if(!command.equals("STOP")) {
                 System.out.println("Shutting down...");
                 exit(0);
