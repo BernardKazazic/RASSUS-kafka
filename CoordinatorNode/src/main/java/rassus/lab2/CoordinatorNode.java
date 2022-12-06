0b6bbc2f19463cef1363ba43ed4803d6d0c9b193
Bernard Kazazić <bernard.kazazic@commondo.eu>
2022-12-06 16:45:25 +0100

    CoordinatorNode producing json messages instead of string

--- CoordinatorNode/src/main/java/rassus/lab2/CoordinatorNode.java
+++ CoordinatorNode/src/main/java/rassus/lab2/CoordinatorNode.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.json.JSONObject;
 
 import java.util.Properties;
 import java.util.Scanner;
@@ -18,9 +19,9 @@ public class CoordinatorNode {
         Properties producerProperties = new Properties();
         producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomJSONSerializer.class);
 
-        Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<>(producerProperties);
+        Producer<String, JSONObject> producer = new org.apache.kafka.clients.producer.KafkaProducer<>(producerProperties);
 
         Scanner sc = new Scanner(System.in);
 
@@ -32,8 +33,12 @@ public class CoordinatorNode {
                 System.out.println("Valid commands are:\nSTART - starts a group of nodes\nSTOP - stops and shuts down all nodes\n\n");
                 continue;
             }
+            System.out.println("Sending " + command + " command.");
 
-            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, null, command);
+            JSONObject jsonCommand = new JSONObject();
+            jsonCommand.put("command", command);
+
+            ProducerRecord<String, JSONObject> record = new ProducerRecord<>(TOPIC, null, jsonCommand);
 
             producer.send(record);
             producer.flush();
